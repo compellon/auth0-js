b1cb43fd8eaad4ef75521f0d0692ec9e11a9a312
John McLaughlin <jmclaughlin@compellon.com>
2020-05-08 16:22:48 -0700

    Build dist changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const BabiliPlugin = require('babili-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'auth0.js': './src/index.js',
+    'auth0.min.js': './src/index.js'
+  },
   output: {
-    filename: 'compellon-auth0-js.js',
+    filename: '[name]',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new BabiliPlugin(
+      {},
+      {
+        test: /\.min\.js$/
+      }
+    )
+  ]
 };
+
+// const path = require('path');
+//
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'auth0.js',
+//     path: path.resolve(__dirname, 'dist')
+//   }
+// };
